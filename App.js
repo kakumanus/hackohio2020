c4e26a92180c05b785d272271ab4167914a6b1ed
rkOSU <46794263+rkOSU@users.noreply.github.com>
2020-11-15 09:35:36 -0500

    zone info handling

--- App.js
+++ App.js
@@ -3,12 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import {BottomMenu} from "./src/components/BottomMenu/BottomMenu";
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as d3 from 'd3-geo';
-import region from "./data/cincinnati_censustracts";
+
+/*
+const sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('../../data/Delivery', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+
+});
+
+*/
 
 export default function App() {
-  var zone_info= get_zone_id([-84.325483, 39.364387]);
-  console.log(zone_info);
+  
   return (
     <NavigationContainer>
       <SafeAreaProvider>
@@ -18,26 +27,6 @@ export default function App() {
   );
 }
 
-
-function get_zone_id(coords){
-  var zone;
-  
-  for (zone of region.features){
-    //console.log(zone);
-    if (d3.geoContains(zone, coords)){
-      return [zone.properties.MOVEMENT_ID, zone.properties.DISPLAY_NAME];
-    }
-  }
-
-   /* TESTS
-  var zone = region.features[0];
-  var centroid = d3.geoCentroid(zone);                        //-TRUE
-  console.log(d3.geoContains(zone, centroid));                //-TRUE
-  console.log(d3.geoContains(zone, [-84.344625, 39.311512])); //-TRUE
-  console.log(d3.geoContains(zone, [-84.347761, 39.302136])); //-TRUE
-  console.log(d3.geoContains(zone, [-84.357926, 39.303484])); //-FALSE
-  */
-}
 
 
 const styles = StyleSheet.create({
--- src/screens/DeliveryInfoScreen.tsx
+++ src/screens/DeliveryInfoScreen.tsx
@@ -1,16 +1,97 @@
 import React from "react";
 import {LinearGradient} from 'expo-linear-gradient'
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, ImageBackground } from "react-native";
+import * as d3 from 'd3-geo';
+import region from "../../data/cincinnati_censustracts";
+/*
+//import { openDatabase } from 'react-native-sqlite-storage';
+import * as SQLite from 'expo-sqlite';
+
+// Check if the directory where we are going to put the database exists
+let dirInfo;
+try {
+    dirInfo = await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}SQLite`);
+} catch(err) { Sentry.captureException(err) };
+  
+if (!dirInfo.exists) {
+  try {
+    await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}SQLite`, { intermediates: true });
+  } catch(err) { Sentry.captureException(err) }
+};
+
+// Downloads the db from the original file
+// The db gets loaded as read only
+FileSystem.downloadAsync(
+  Asset.fromModule(require('../databases/programs.db')).uri,
+    `${FileSystem.documentDirectory}SQLite/programs${version}.db`
+  ).then(() => {
+  programsDB = SQLite.openDatabase(`programs${version}.db`); // We open our downloaded db
+  loadDB(loaded); // We load the db into the persist store
+}).catch(err => Sentry.captureException(err));
+**/
+
+function get_zone_info(coords){
+  var zone;
+  
+  for (zone of region.features){
+    //console.log(zone);
+    if (d3.geoContains(zone, coords)){
+      return [zone.properties.MOVEMENT_ID, zone.properties.DISPLAY_NAME];
+    }
+  }
+
+
+   /* TESTS
+  var zone = region.features[0];
+  var centroid = d3.geoCentroid(zone);                        //-TRUE
+  console.log(d3.geoContains(zone, centroid));                //-TRUE
+  console.log(d3.geoContains(zone, [-84.344625, 39.311512])); //-TRUE
+  console.log(d3.geoContains(zone, [-84.347761, 39.302136])); //-TRUE
+  console.log(d3.geoContains(zone, [-84.357926, 39.303484])); //-FALSE
+  */
+}
+
+function calculate_mean_times(zone1, zone2){
+  var query = 'SELECT Mean_Travel_Time AS mean FROM All_Hourly_Aggregate WHERE Source_ID = ? AND Dest_ID = ? ORDER BY Mean_Travel_Time DESC LIMIT 3;';
+  let arr = [];
+ 
+
+  /*
+  db.transaction(
+    tx => {
+      tx.executeSql(query, [zone1, zone2], (trans, result) => {
+        console.log(trans, result);
+        console.log('hi');
+      });
+    }
+  );
+    return arr;
+
+  */
+  }
+
+
 
 export const DeliveryInfoScreen = ({route, navigation}) => {
 
   const { source, dest } = route.params;
+
+  var source_coords = [source.lng, source.lat];
+  var dest_coords = [dest.lng, dest.lat];
+
+  var source_info= get_zone_info(source_coords);
+  var dest_info = get_zone_info(dest_coords);
+
+  //console.log(source_info);
+
+  var arr = calculate_mean_times(source_info[0], dest_info[0]);
+
   return (
       <ImageBackground source={{uri: 'https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2019/05/Screenshot-2019-05-08-22.21.20.png'}}
         style={styles.backImg}>
         <ScrollView style={styles.container}> 
-          <Text>{JSON.stringify(source)}</Text>
-          <Text>{JSON.stringify(dest)}</Text>
+          <Text>{}</Text>
+          <Text>{dest_info[0]}</Text>
         </ScrollView>
       </ImageBackground>
   );
